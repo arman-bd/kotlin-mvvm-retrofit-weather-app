0241d5ba67aa8e0211fd3d006c546cb119e76ef0
Arman Hossain <arman.hossain.bd93@gmail.com>
2020-10-17 23:36:10 +0600

    Update MainActivity.kt

--- app/src/main/java/mvvm/kotlin/weather/Activities/MainActivity.kt
+++ app/src/main/java/mvvm/kotlin/weather/Activities/MainActivity.kt
@@ -86,6 +86,10 @@ class MainActivity : AppCompatActivity(), CellClickListener  {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(s: Editable) {}
         })
+
+        if(currentWoeid != 0){
+            weatherViewModel!!.requestWeatherData(currentWoeid);
+        }
     }
 
     override fun locationOnClick(pos: Int, woeid: Int) {
